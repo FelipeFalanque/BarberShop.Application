d13bc7e2d725c4563a42b47635e3aa3279a0051f
Cesar Falanque <cesar.falanque@grupoboticario.com.br>
2023-12-14 20:05:43 -0300

    Configuracao de funcionamento

--- wwwroot/js/management/management.daywork.v1.0.js
+++ wwwroot/js/management/management.daywork.v1.0.js
@@ -1,13 +1,46 @@
 
+var codeDayWork = null;
 $(document).ready(function () {
 
 
 });
-function OpenModalAppointment() {
+function OpenModalDayWork(code, description, start, end, open) {
 
+    $("#modalDayWorkBodyLabel").text(description);
 
-    let modal = bootstrap.Modal.getOrCreateInstance(document.getElementById('modalAppointment')) // Returns a Bootstrap modal instance
+    $("#radioOpen").prop("checked", open);
+    $("#radioClose").prop("checked", !open);
+
+    $("#selectStartHour").val(start);
+    $("#selectLastHour").val(end);
+
+    let modal = bootstrap.Modal.getOrCreateInstance(document.getElementById('modalDayWork')) // Returns a Bootstrap modal instance
     // Show or hide:
     modal.show();
     //modal.hide();
+
+    codeDayWork = code;
+}
+
+function ConfirmButton() {
+
+    let code = codeDayWork
+    let isOpenSelected = $("#radioOpen").is(":checked");
+
+    if (isOpenSelected) {
+        $(`#txtOpenClose_${code}`).removeClass('text-danger');
+        $(`#txtOpenClose_${code}`).addClass('text-success');
+        $(`#txtOpenClose_${code}`).text('ABERTO');
+    } else {
+        $(`#txtOpenClose_${code}`).removeClass('text-success');
+        $(`#txtOpenClose_${code}`).addClass('text-danger');
+        $(`#txtOpenClose_${code}`).text('FECHADO');
+    }
+
+    let selectStartHour = $("#selectStartHour").val();
+    let selectLastHour = $("#selectLastHour").val();
+
+    $(`#txtOpen_${code}`).text(selectStartHour);
+    $(`#txtClose_${code}`).text(selectLastHour);
+
 }
